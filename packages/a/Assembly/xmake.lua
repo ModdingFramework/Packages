db6681db87a0f4990d3eb63e51c8711fa21bc110
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-09 21:04:16 -0700

    Add deps for Assembly package

--- packages/a/Assembly/xmake.lua
+++ packages/a/Assembly/xmake.lua
@@ -3,6 +3,10 @@ package("Assembly")
     set_homepage("https://github.com/ModdingFramework/Assembly.h")
     set_description("A header-only library for generating/disassembling assembly.")
     add_urls("https://github.com/ModdingFramework/Assembly.h.git")
+
+    add_deps("xbyak")
+    add_deps("Zydis")
+
     on_install(function (package)
         os.cp("include", package:installdir())
     end)
