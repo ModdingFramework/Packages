f94867e4692191244f3ccf63943432b007c669f8
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-09 21:04:26 -0700

    Add deps for Memory package

--- packages/m/Memory/xmake.lua
+++ packages/m/Memory/xmake.lua
@@ -3,6 +3,10 @@ package("Memory")
     set_homepage("https://github.com/ModdingFramework/Memory.h")
     set_description("A header-only library for reading/writing memory.")
     add_urls("https://github.com/ModdingFramework/Memory.h.git")
+
+    add_deps("Logging")
+    add_deps("StringFormatting")
+
     on_install(function (package)
         os.cp("include", package:installdir())
     end)
