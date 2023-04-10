0ffcd102bbf09b2af168d3646828c4681a54d772
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-09 22:53:11 -0700

    s/add_deps/add_requires/g

--- packages/a/Assembly/xmake.lua
+++ packages/a/Assembly/xmake.lua
@@ -1,11 +1,9 @@
 package("Assembly")
-    set_kind("library")
     set_homepage("https://github.com/ModdingFramework/Assembly.h")
     set_description("A header-only library for generating/disassembling assembly.")
     add_urls("https://github.com/ModdingFramework/Assembly.h.git")
 
-    add_deps("xbyak")
-    add_deps("Zydis")
+    add_requires("xbyak", "zydis")
 
     on_install(function (package)
         os.cp("include", package:installdir())
--- packages/m/Memory/xmake.lua
+++ packages/m/Memory/xmake.lua
@@ -1,11 +1,9 @@
 package("Memory")
-    set_kind("library")
     set_homepage("https://github.com/ModdingFramework/Memory.h")
     set_description("A header-only library for reading/writing memory.")
     add_urls("https://github.com/ModdingFramework/Memory.h.git")
 
-    add_deps("Logging")
-    add_deps("StringFormatting")
+    add_requires("Logging", "StringFormatting")
 
     on_install(function (package)
         os.cp("include", package:installdir())
